25385109c0841a16a7d2edcac1484b3769562e65
Mustapha Elzehary <mustapha.elzehary@gmail.com>
2022-03-30 08:13:54 +0200

    Training

---
+++ Source/Sultan/Ui/Main/SW_MainOverView.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SW_MainOverView.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SULTAN_API USW_MainOverView : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
